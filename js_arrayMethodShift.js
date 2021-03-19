6bb4357444a18e7707ced86b336ecf43efbc94ba
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2021-03-20 01:15:50 +0200

    Solution

--- js_arrayMethodShift.js
+++ js_arrayMethodShift.js
@@ -1,1 +1,17 @@
 'use strict';
+
+numbers.shift = function() {
+  if (this.length === 0) {
+    return undefined;
+  }
+
+  const removeElement = this[0];
+
+  for (let i = 1; i < this.length; i++) {
+    this[i - 1] = this[i];
+  }
+
+  this.length -= 1;
+
+  return removeElement;
+};
